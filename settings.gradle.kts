6de7ce92091f976750614627e10f19118f4d4a30
Robinson <robinson@dorkbox.com>
2021-04-26 02:25:31 +0200

    gradle debug info/settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,3 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// awful hack in settings.grade to reflectively enable logging
+//val LoggerFactory = Class.forName("org.slf4j.LoggerFactory")
+//val OutputEventListenerBackedLoggerContext = Class.forName("org.gradle.internal.logging.slf4j.OutputEventListenerBackedLoggerContext")
+//val OutputEventListener = Class.forName("org.gradle.internal.logging.events.OutputEventListener")
+//val StandardOutputListener = Class.forName("org.gradle.api.logging.StandardOutputListener")
+//val StreamBackedStandardOutputListener = Class.forName("org.gradle.internal.logging.text.StreamBackedStandardOutputListener")
+//val StyledTextOutput = Class.forName("org.gradle.internal.logging.text.StyledTextOutput")
+//val StreamingStyledTextOutput = Class.forName("org.gradle.internal.logging.text.StreamingStyledTextOutput")
+//val StyledTextOutputBackedRenderer = Class.forName("org.gradle.internal.logging.console.StyledTextOutputBackedRenderer")
+//
+//val newStreamBackedStandardOutputListener = StreamBackedStandardOutputListener.getDeclaredConstructor(java.io.OutputStream::class.java)
+//
+//val newStreamingStyledTextOutput = StreamingStyledTextOutput.getDeclaredConstructor(StandardOutputListener)
+//val newStyledTextOutputBackedRenderer = StyledTextOutputBackedRenderer.getDeclaredConstructor(StyledTextOutput)
+//
+//val gradleLoggerFactory = LoggerFactory.getDeclaredMethod("getILoggerFactory").invoke(null)
+//OutputEventListenerBackedLoggerContext.getDeclaredMethod("setLevel", LogLevel::class.java).invoke(gradleLoggerFactory, LogLevel.DEBUG)
+//
+//val streamBackedStandardOutputListener = newStreamBackedStandardOutputListener.newInstance(System.out)
+//val streamingStyledTextOutput = newStreamingStyledTextOutput.newInstance(streamBackedStandardOutputListener)
+//val styledTextOutputBackedRenderer = newStyledTextOutputBackedRenderer.newInstance(streamingStyledTextOutput)
+//OutputEventListenerBackedLoggerContext.getDeclaredMethod("setOutputEventListener", OutputEventListener).invoke(gradleLoggerFactory, styledTextOutputBackedRenderer)
+
