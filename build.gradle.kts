0465493d6f8df74f7a913fa34a1e7b83af1552bc
Robinson <robinson@dorkbox.com>
2022-01-09 20:33:58 +0100

    Updated gradle, updated build dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,10 +25,10 @@ import java.time.Instant
 gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show the stacktrace!
 
 plugins {
-    id("com.dorkbox.GradleUtils") version "2.8"
-    id("com.dorkbox.Licensing") version "2.7"
-    id("com.dorkbox.VersionUpdate") version "2.3"
-    id("com.dorkbox.GradlePublish") version "1.11"
+    id("com.dorkbox.GradleUtils") version "2.16"
+    id("com.dorkbox.Licensing") version "2.10"
+    id("com.dorkbox.VersionUpdate") version "2.4"
+    id("com.dorkbox.GradlePublish") version "1.12"
 
     kotlin("jvm") version "1.4.32"
 }
@@ -48,7 +48,6 @@ object Extras {
 
     val buildDate = Instant.now().toString()
 }
-
 
 ///////////////////////////////
 /////  assign 'Extras'
